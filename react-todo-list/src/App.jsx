d5a3e28147f5c6e39adc20988585087446f9ebf3
isurudotxyz <agisuru@icloud.com>
2025-01-01 14:37:17 +0100

    initialized app

---
+++ react-todo-list/src/App.jsx
@@ -0,0 +1,12 @@
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return;
+  <main>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+  </main>;
+}
+
+export default App;
